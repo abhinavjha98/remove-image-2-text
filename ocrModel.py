0854ad7fccfbe53e5135e2c8140d8f03a3aa8e84
Abhinav Jha <abhinavjha98ald@gmail.com>
2021-04-04 23:12:33 +0530

    Modilfied the file

--- ocrModel.py
+++ ocrModel.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import cv2
 import easyocr
 from pylab import rcParams
+import re
+import shutil, os
 
 rcParams['figure.figsize'] = 8, 16
 
@@ -21,17 +23,55 @@ import torch
 # the_model = torch.load("Pickle_RL_Model.h5")
 # the_model
 
+import os
 
-output = reader.readtext('q.jpg')
-
-
-img = cv2.imread('q.jpg')
-for detection in output:
-  top_left = tuple([int(val) for val in detection[0][0]])
-  bottom_right = tuple([int(val) for val in detection[0][2]])
+for file_name in os.listdir("E:/IntechHub Solution/modelOCR/"):
   
-  img = cv2.rectangle(img,top_left,bottom_right,(255,255,255),-1)
+    if file_name.split(".")[-1].lower() in {"jpeg", "jpg", "png"}:
+        output = reader.readtext(file_name)
+        count = 0
+        for detection in output:
+          text = detection[1]
+          count += len(re.findall(r'\w+', text))
+          print(count)
+        if count < 60:
+          img = cv2.imread(file_name)
+          for detection in output:
+            top_left = tuple([int(val) for val in detection[0][0]])
+            bottom_right = tuple([int(val) for val in detection[0][2]])
+            
+            img = cv2.rectangle(img,top_left,bottom_right,(255,255,255),-1)
+            # plt.figure(figsize=(10,10))
+            # cv2.imshow("img",img)
+            # cv2.waitKey(0)
+            
+          shutil.move(file_name,'images/')
+        else:
+          shutil.move(file_name,'documents/')
+          print("pass")
+        print(file_name)
 
-plt.figure(figsize=(10,10))
-cv2.imshow("img",img)
-cv2.waitKey(0)
+# output = reader.readtext('q1.jpeg')
+# count = 0
+# for detection in output:
+  
+#   text = detection[1]
+#   count += len(re.findall(r'\w+', text))
+#   print(count)
+
+# if count < 60:
+#   img = cv2.imread('q.jpg')
+#   for detection in output:
+#     top_left = tuple([int(val) for val in detection[0][0]])
+#     bottom_right = tuple([int(val) for val in detection[0][2]])
+    
+#     img = cv2.rectangle(img,top_left,bottom_right,(255,255,255),-1)
+#     # plt.figure(figsize=(10,10))
+#     # cv2.imshow("img",img)
+#     # cv2.waitKey(0)
+#     files="q.jpg"
+#     shutil.move(files,'images/')
+# else:
+#   files = "q1.jpeg"
+#   shutil.move(files,'documents/')
+#   print("pass")
